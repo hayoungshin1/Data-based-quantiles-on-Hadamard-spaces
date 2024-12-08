f756780d58805c54d9cf8bf493a7cf5974b89942
hayoungshin1 <64515255+hayoungshin1@users.noreply.github.com>
2024-12-08 21:40:28 +0900

    Update hypexperiments.py

--- hypexperiments.py
+++ hypexperiments.py
@@ -1,3 +1,5 @@
+# approximation comparisons
+
 m=64
 
 betas=[0.2,0.4,0.6,0.8,0.98]
@@ -42,3 +44,51 @@ for i in range(4):
     for j in range(2):
         for k in range(len(betas)):
             print('%.4f'%(torch.mean(newdist(allquantiles[3*i][(k*m+1):((k+1)*m+1)],allquantiles[3*i+j+1][(k*m+1):((k+1)*m+1)])).item()))
+
+# permutation test
+
+m=4
+
+betas=[0.5]
+angles=[k*2*np.pi/m for k in range(m)]
+xis=[torch.tensor([[np.cos(angles[k]),np.sin(angles[k])]]) for k in range(m)]
+
+np.random.seed(900)
+x1=np.random.normal(0,0.3,(100,2))
+x1=torch.Tensor(x1)
+x2=x1.detach().clone()
+x2[:,1]/=4
+x1=B2H(x1)
+x2=B2H(x2)
+x1quantiles=quantile(x1, 0, xis[0], 'd', 1)
+x2quantiles=quantile(x2, 0, xis[0], 'd', 1)
+medstat=torch.sum(newdist(x1quantiles,x2quantiles)).item()
+for i in range(len(betas)):
+    for j in range(len(xis)):
+        x1quantiles=torch.concat((x1quantiles,quantile(x1, betas[i], xis[j], 'd', 1)),dim=0)
+        x2quantiles=torch.concat((x2quantiles,quantile(x2, betas[i], xis[j], 'd', 1)),dim=0)
+
+quantstat=torch.sum(newdist(x1quantiles,x2quantiles)).item()
+print(medstat,quantstat)
+
+medsum=0
+quantsum=0
+totalx=torch.concat((x1,x2),0)
+reps=500
+for k in range(reps):
+    permindices=np.random.choice(totalx.shape[0],x1.shape[0],replace=False)
+    permx1=totalx[permindices,:]
+    permx2=np.delete(totalx,permindices,axis=0)
+    permx1quantiles=quantile(permx1, 0, xis[0], 'd', 1)
+    permx2quantiles=quantile(permx2, 0, xis[0], 'd', 1)
+    permmedstat=torch.sum(newdist(permx1quantiles,permx2quantiles)).item()
+    for i in range(len(betas)):
+        for j in range(len(xis)):
+            permx1quantiles=torch.concat((permx1quantiles,quantile(permx1, betas[i], xis[j], 'd', 1)),dim=0)
+            permx2quantiles=torch.concat((permx2quantiles,quantile(permx2, betas[i], xis[j], 'd', 1)),dim=0)
+    permquantstat=torch.sum(newdist(permx1quantiles,permx2quantiles)).item()
+    medsum+=(permmedstat>=medstat)
+    quantsum+=(permquantstat>=quantstat)
+    print(k,medsum,quantsum)
+
+print(medsum/reps,quantsum/reps)
